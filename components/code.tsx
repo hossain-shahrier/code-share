1c165e124ea3d45e91aa8102dc89b6612100bd64
Shahrier Hossain <hossainshahrier.shm@gmail.com>
2024-01-01 02:44:13 +0600

    Initialization

---
+++ components/code.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { useState } from 'react';
+
+interface CodeProps {
+  placeholder: string;
+  initialValue?: string;
+}
+const Code = () => {
+  return <div>Code</div>;
+};
+
+export default Code;
